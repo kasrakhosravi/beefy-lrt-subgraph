e6b31bb9d8551458fddd3eeb450dba5f36e1df97
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-04-17 21:53:24 +0200

    Fix linea

--- src/utils/ppfs.ts
+++ src/utils/ppfs.ts
@@ -1,23 +1,22 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
 import { Token } from "../../generated/schema"
-import { EIGHTEEN_BI, ONE_ETH_BD, ONE_ETH_BI } from "./decimal"
+import { EIGHTEEN_BI, ONE_ETH_BD, ONE_ETH_BI, TEN_BI } from "./decimal"
 
 export function ppfsToShareRate(ppfs: BigInt, underlyingToken: Token): BigDecimal {
-  // todo: account for underlying token decimals different than 18
-  if (underlyingToken.decimals.notEqual(EIGHTEEN_BI)) {
-    throw new Error("Not implemented yet")
+  // fast path for 18 decimals
+  if (underlyingToken.decimals.equals(EIGHTEEN_BI)) {
+    return ppfs.toBigDecimal().div(ONE_ETH_BD)
   }
-  return ppfs.toBigDecimal().div(ONE_ETH_BD)
+
+  const divisor = TEN_BI.pow(underlyingToken.decimals.plus(EIGHTEEN_BI)).toBigDecimal()
+  return ppfs.toBigDecimal().times(ONE_ETH_BD).div(divisor)
 }
 
-export function rawShareBalanceToRawUnderlyingBalance(
-  ppfs: BigInt,
-  rawSharesBalance: BigInt,
-  underlyingToken: Token,
-): BigInt {
-  // todo: account for underlying token decimals different than 18
-  if (underlyingToken.decimals.notEqual(EIGHTEEN_BI)) {
-    throw new Error("Not implemented yet")
+export function rawShareBalanceToRawUnderlyingBalance(ppfs: BigInt, rawSharesBalance: BigInt, underlyingToken: Token): BigInt {
+  // fast path for 18 decimals
+  if (underlyingToken.decimals.equals(EIGHTEEN_BI)) {
+    return rawSharesBalance.times(ppfs).div(ONE_ETH_BI)
   }
-  return rawSharesBalance.times(ppfs).div(ONE_ETH_BI)
+
+  return rawSharesBalance.times(ppfs).div(TEN_BI.pow(underlyingToken.decimals))
 }
