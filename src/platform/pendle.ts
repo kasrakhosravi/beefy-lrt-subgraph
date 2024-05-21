f8ea631479254aa570edc06579fbd0dcb3a7547b
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-05-21 21:14:33 +0200

    Fix linea start block

--- src/platform/pendle.ts
+++ src/platform/pendle.ts
@@ -3,7 +3,6 @@ import { TokenBalance } from "./common"
 import { PendleMarket as PendleMarketContract } from "../../generated/templates/BeefyVaultV7/PendleMarket"
 import { PendleSyToken as PendleSyTokenContract } from "../../generated/templates/BeefyVaultV7/PendleSyToken"
 import { Address } from "@graphprotocol/graph-ts"
-import { getTokenAndInitIfNeeded } from "../entity/token"
 
 const PENDLE_ROUTER_ADDRESS = Address.fromString("0x00000000005BBB0EF59571E58418F9a4357b68A0")
 
@@ -11,10 +10,9 @@ export function getVaultTokenBreakdownPendle(vault: BeefyVault): Array<TokenBala
   let balances = new Array<TokenBalance>()
 
   const wantTotalBalance = vault.rawUnderlyingBalance
-  const underlyingToken = getTokenAndInitIfNeeded(vault.underlyingToken)
 
   // fetch on chain data
-  const pendleMarketContract = PendleMarketContract.bind(Address.fromBytes(underlyingToken.id))
+  const pendleMarketContract = PendleMarketContract.bind(Address.fromBytes(vault.underlyingToken))
   const tokenAddresses = pendleMarketContract.readTokens()
   const pendleState = pendleMarketContract.readState(PENDLE_ROUTER_ADDRESS)
   const syTokenContract = PendleSyTokenContract.bind(tokenAddresses.value0)
