22e3a606fcbaf8b05676ec77ba4eab656fad2cae
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2024-07-23 16:53:36 -0400

    Add Camelot rsETH

--- src/vault-config.ts
+++ src/vault-config.ts
@@ -62,6 +62,11 @@ export function getChainVaults(): Array<VaultConfig> {
         "0x04D463bf08dF252Cb09a87D6d41a33c535942710",
       ]),
     )
+    vaults.push(
+      new VaultConfig("camelot-rseth-weth", PLATFORM_BEEFY_CLM, "0xac8246F01197fB783Bf1A80960821835045Ec680", [
+        "0xD9001574E23fb909657A25494f540A9B3804b16e",
+      ]),
+    )
   }
 
   if (network === "base") {
